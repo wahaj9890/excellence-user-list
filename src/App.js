ec9a334ae96d6d82a4f2248130adc38316779c77
Wahajuddin <wahaj9890@gmail.com>
2021-07-22 17:35:02 -0700

    todo commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+import "./style.css";
+import Card from "@material-ui/core/Card";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      offset: 0,
+      data: [],
+      perPage: 6,
+      pageNumber: 0,
+      currentPage: 0,
+    };
+    this.handlePageClick = this.handlePageClick.bind(this);
+  }
+  receivedData() {
+    axios
+      .get(`https://reqres.in/api/users?page=${this.state.currentPage + 1}`)
+      .then((res) => {
+        const postData = res.data.data.map((user) => (
+          <React.Fragment>
+            <div className="cardContainer">
+              <img src={user.avatar} alt="" className="userImage" />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+              <p>
+                <b>UserName:</b>
+                {user.first_name + "" + user.last_name}
+              </p>
+              <p>
+                <b>Email:</b>
+                {user.email}
+              </p>
+              <hr />
+            </div>
+          </React.Fragment>
+        ));
+
+        this.setState({
+          pageCount: Math.ceil(res.data.total / this.state.perPage),
+
+          postData,
+        });
+      });
+  }
+  handlePageClick = (e) => {
+    debugger;
+    const selectedPage = e.selected;
+
+    const offset = selectedPage * this.state.perPage;
+
+    this.setState(
+      {
+        currentPage: selectedPage,
+        offset: offset,
+        pageNumber: selectedPage,
+      },
+      () => {
+        this.receivedData();
+      }
+    );
+  };
+
+  componentDidMount() {
+    this.receivedData();
+  }
+  render() {
+    return (
+      <div>
+        {this.state.postData}
+        <ReactPaginate
+          previousLabel={"prev"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
